e145438324ee1b632064dcf604fdedf984491387
whj1995 <wu38607@live.com>
2019-04-17 22:41:30 +0800

    rxjs

--- example/store/actions.ts
+++ example/store/actions.ts
@@ -1,19 +1,24 @@
 // tslint:disable: no-console
 import { ajax } from 'rxjs/ajax';
-import { filter, mergeMap, takeUntil, tap } from 'rxjs/operators';
+import { mergeMap, takeUntil, tap, throttleTime } from 'rxjs/operators';
 import { ICtx } from './index';
 
 export function fetchCount(ctx: ICtx<number>, _d: number): any {
   return ctx.curAction$.pipe(
+    throttleTime(5000),
     tap(() => ctx.commit('loading', 'fetching...')),
     mergeMap(
       () => ajax.getJSON('https://api.github.com/users/whj1995/repos')
         .pipe(
-          tap((result: any) => ctx.commit('set', result.length)),
+          tap((result: any) => ctx.dispatch('sett', result.length)),
           takeUntil(ctx.createAction$('cancel').pipe(tap((x) => console.log(x))))
         )
     )
   );
+}
+
+export function sett(ctx: ICtx<number>, _: number) {
+  return ctx.curAction$.pipe(tap((d) => ctx.commit('set', d)));
 }
 
 export function cancel(ctx: ICtx<any>, _d: string) {
